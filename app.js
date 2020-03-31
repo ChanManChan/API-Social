d4246743fdbfeda2328cda9705833fd3f31c8d1a
Nanda Gopal <ngopal253@gmail.com>
2020-03-31 17:53:47 +0530

    route changed for remote API

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 
 // API Docs
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   fs.readFile('docs/apiDocs.json', (err, data) => {
     if (err) {
       res.status(500).json({
@@ -51,9 +51,9 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 app.use(cors());
-app.use('/', postRoutes);
-app.use('/', authRoutes);
-app.use('/', userRoutes);
+app.use('/api', postRoutes);
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
 app.use(function(err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401).json({ error: 'Unauthorized!' });
